faa9724e62951c53efff26f085c1e9119534498e
Nguyễn Anh Đức <61057010+ducna0809@users.noreply.github.com>
2020-02-16 22:18:06 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -28,7 +28,7 @@ function firstQuestion(){
 
  // switch button position
  function switchButton() {
-    var audio = new Audio('../sound/duck.mp3');
+    var audio = new Audio('sound/duck.mp3');
     audio.play();
     var leftNo = $('#no').css("left");
     var topNO = $('#no').css("top");
@@ -38,14 +38,18 @@ function firstQuestion(){
     $('#no').css("top", topY);
     $('#yes').css("left", leftNo);
     $('#yes').css("top", topNO);
-    console.log('kk');
 }
 // move random button póition
 function moveButton() {
-    var audio = new Audio('../sound/Swish1.mp3');
+    var audio = new Audio('sound/Swish1.mp3');
     audio.play();
-    var x = Math.random() * 500;
-    var y = Math.random() * 500;
+    if (screen.width<=600) {
+        var x = Math.random() * 300;
+        var y = Math.random() * 500;
+    } else{
+        var x = Math.random() * 500;
+        var y = Math.random() * 500;
+    }
     var left = x + 'px';
     var top = y + 'px';
     $('#no').css("left", left);
@@ -61,6 +65,10 @@ $('#no').mousemove(function() {
         moveButton();
     n++;
 });
+$('#no').click(() => {
+    if (screen.width>=900)
+        switchButton();
+})
 
 // generate text in input
 function textGenerate() {
